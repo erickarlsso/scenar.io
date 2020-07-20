aff3cc1ecfe82f54a5ab9e9c92b125fec440d691
Eric karlsson <erickarlsso@users.noreply.github.com>
2020-07-20 20:14:17 +0200

    Updates

---
+++ precache-manifest.206b957777be23ddd756ccfc42c75ae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031b22c72a15472b0775c668a5891e34",
+    "url": "/scenar.io/index.html"
+  },
+  {
+    "revision": "08aa47d86ae9d7cc4a29",
+    "url": "/scenar.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8adb645de035a7d3e18e",
+    "url": "/scenar.io/static/js/2.825012b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scenar.io/static/js/2.825012b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08aa47d86ae9d7cc4a29",
+    "url": "/scenar.io/static/js/main.a1826849.chunk.js"
+  },
+  {
+    "revision": "aea32e3d831adff2b52e",
+    "url": "/scenar.io/static/js/runtime-main.6bd1aac4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scenar.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
